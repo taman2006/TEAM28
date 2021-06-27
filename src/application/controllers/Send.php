37224567fc0e25eee30b9760b7c3bb72711b2e2d
ro-oa <uaki08280420@icloud.com>
2021-06-27 11:40:29 +0900

    コントローラとモデル変更

--- src/application/controllers/Send.php
+++ src/application/controllers/Send.php
@@ -7,9 +7,31 @@ class Send extends CI_Controller {
   
   public function index(){
 
-    $this->load->model('send_message');
+    $this->load->model('get_data');
+    $array = $this->get_data->get_data();
 
-    $this->send_message->post_message('hello');
+    foreach($array as $vals){
+      $time = $this->get_data->convertToDayTimeAgo($vals['limit_date']);
+      if($time < 4 && $time > 0){
+            echo '課題名：'.$vals['kadai_name'].'  / 期限：'.$vals['limit_date'].'  / '.$time.'日前';
+            echo '<br>';
+          }
+    }     
+        
+   
+
+    foreach($array as $vals){
+      $this->load->model('get_data');
+      $time = $this->get_data->convertToDayTimeAgo($vals['limit_date']);
+      if($time < 4 && $time > 0){
+            $this->load->model('send_message');
+            $this->send_message->post_message('課題名：'.$vals['kadai_name'].'  / 期限：'.$vals['limit_date'].'  / '.$time.'日前');
+      }
+   }   
+        
+  
+    
+
   }
 
 }
